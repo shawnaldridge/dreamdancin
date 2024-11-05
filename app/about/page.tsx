a3d9de7d88f7abb4134103f22bd90c02413651a7
Shawn Aldridge <raskolnikov42@gmail.com>
2024-11-04 19:00:17 -0800

    escape character again

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="flex min-h-screen p-24 flex-row grid-cols-02 place-content-evenly">
       <div className="flex basis-1/2">
-        A paragraph about us, this is what we're up to here on the farm.
+        A paragraph about us, this is what we&apos;re up to here on the farm.
       </div>
       <div className="flex basis-1/2">
         <Image
