e7d4773d69388b5be313281d7c51b19c97224044
Shawn Aldridge <raskolnikov42@gmail.com>
2025-04-21 23:57:45 -0700

    Landing section

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ export default function Home() {
 
         <div className="basis-1/2 flex-col">
           <p className="col-span-1">
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Placerat scelerisque augue praesent nulla ultrices nascetur sit at mattis. Feugiat elementum varius facilisis pulvinar efficitur. Primis in ante lectus leo elit neque pharetra. Cras sodales vitae commodo maecenas nascetur ac. Arutrum a; nulla per finibus tortor ad potenti. 
+            Welcome! We are a small but growing farm just outside of Eugene, Oregon.
           </p>
           <br/>
           <p className="">
-            Morbi malesuada conubia dictum vel parturient. Cubilia gravida potenti efficitur senectus, neque iaculis tristique. Consequat velit tellus egestas vestibulum eu magnis porta. Mauris per rhoncus primis dapibus eget aliquet diam ultricies venenatis. Ut odio venenatis finibus vestibulum eleifend lacus. 
+            We raise and show dairy goats! Specifically ADGA-registered Nigerian Dwarf goats. 
           </p>
           <br/>
           <p className="">
-            Condimentum risus tristique vehicula montes dui mattis fringilla ante dictum. Fusce aptent a morbi per; porta facilisi nam. Sagittis sem vulputate tellus eleifend lacinia iaculis. Laoreet ullamcorper ex habitant quam, ex mi risus. Neque consectetur mollis nulla donec aptent finibus a amet. 
+            We also raise and sell Coturnix quail, including hatching eggs, as well as live and processed birds and eggs for both breeding and feeding. 
           </p>
 
         </div>
